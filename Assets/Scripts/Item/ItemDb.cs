cfe6b4ffd82122d1128cd7fd8a1d75d57f5a7d76
Lydia Dover <doverlydia@gmail.com>
2024-08-26 14:49:03 +0300

    shop editor

--- Assets/Scripts/Item/ItemDb.cs
+++ Assets/Scripts/Item/ItemDb.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 namespace Item
 {
     [CreateAssetMenu(fileName = "ItemDb", menuName = "Items/ItemDb")]
-    class ItemDb : ScriptableObject
+    public class ItemDb : ScriptableObject
     {
-        [SerializeField]
-        SerializedDictionary<string, ItemData> _itemMap;
+        public SerializedDictionary<string, ItemData> ItemMap;
 
         internal ItemData GetItem(string id)
         {
-            if (_itemMap.TryGetValue(id, out var item))
+            if (ItemMap.TryGetValue(id, out var item))
                 return item;
             return null;
         }
---
+++ Assets/Scripts/Shop/ShopModel.cs
@@ -0,0 +1,56 @@
+using AYellowpaper.SerializedCollections;
+using Item;
+using Shop;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+namespace Shop
+{
+    [CreateAssetMenu(fileName = "ShopModel", menuName = "Shop/ShopModel")]
+    class ShopModel : ScriptableObject
+    {
+        [SerializeField]
+        internal ItemDb _itemDb;
+
+        public SerializedDictionary<string, int> Shop;
+    }
+}
+
+[CustomEditor(typeof(ShopModel))]
+public class ShopModelEditor : Editor
+{
+    private int selectedIndex = 0;
+
+    public override void OnInspectorGUI()
+    {
+        ShopModel shopModel = (ShopModel)target;
+
+        DrawDefaultInspector();
+
+        var keys = shopModel?._itemDb?.ItemMap?.Keys.ToList();
+        keys?.RemoveAll(x => shopModel.Shop.ContainsKey(x));
+
+        if (shopModel._itemDb != null)
+        {
+            if (keys?.Count == 0)
+            {
+                EditorGUILayout.HelpBox("All items in shop, no more to add", MessageType.Info);
+            }
+            else
+            {
+                selectedIndex = EditorGUILayout.Popup("Select an Option", selectedIndex, keys.ToArray());
+
+                if (GUILayout.Button("Add Item To Shop"))
+                {
+                    shopModel.Shop.TryAdd(keys[selectedIndex], 0);
+                }
+            }
+        }
+        else
+        {
+            EditorGUILayout.HelpBox("Must have an ItemDb to select shop items from", MessageType.Warning);
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
