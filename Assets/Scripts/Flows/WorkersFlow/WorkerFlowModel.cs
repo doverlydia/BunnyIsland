8fccf9efb56634b9ab4bd731b4833097f50d667b
Lydia Dover <doverlydia@gmail.com>
2025-01-24 04:24:45 +0200

    Update WorkerFlowModel.cs

--- Assets/Scripts/Flows/WorkersFlow/WorkerFlowModel.cs
+++ Assets/Scripts/Flows/WorkersFlow/WorkerFlowModel.cs
@@ -1,10 +1,28 @@
 using FSM;
+using System;
 using UnityEngine;
 
 namespace WorkersFlow
 {
     class WorkerFlowModel : FlowModel
     {
+        //Target and Current Info:
         internal Transform Target;
+        //internal float CurrentCarryWeight;
+
+        internal float CurrentHunger;
+
+        //Base Stats:
+        internal float MoveSpeed;
+        internal float SearchRange;
+        //internal float SwingRate; 
+        internal float StoneGatherRate; //resource unit per swing
+        internal float WoodGatherRate; //resource unit per swing
+        internal float CarryCapacity;
+
+        internal float StomacheSize;
+        internal float HungerRate;
+
+        internal Transform BaseStation; //Transform needs to be the Station's MonoBehaviour
     }
 }
